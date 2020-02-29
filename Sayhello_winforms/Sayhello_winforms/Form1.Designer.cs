d737eade41e2d3819a34e6e3b59be8bbadb6fc39
leeyunhome <leeyunhome@daum.net>
2020-02-29 15:31:45 +0900

    add sayHello_winform

---
+++ Sayhello_winforms/Sayhello_winforms/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Sayhello_winforms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SayHelloButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // SayHelloButton
+            // 
+            this.SayHelloButton.Location = new System.Drawing.Point(154, 102);
+            this.SayHelloButton.Name = "SayHelloButton";
+            this.SayHelloButton.Size = new System.Drawing.Size(123, 23);
+            this.SayHelloButton.TabIndex = 0;
+            this.SayHelloButton.Text = "Say hello";
+            this.SayHelloButton.UseVisualStyleBackColor = true;
+            this.SayHelloButton.Click += new System.EventHandler(this.SayHelloButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Gulim", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox1.Location = new System.Drawing.Point(154, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(274, 53);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Hello there!";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.SayHelloButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SayHelloButton;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Sayhello_winforms/Sayhello_winforms/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Sayhello_winforms
+{
+    public partial class Form1 : Form
+    {
+        Random rand;
+        public Form1()
+        {
+            InitializeComponent();
+            rand = new Random();
+        }
+
+        private byte[] GetRandomBytes(int n)
+        {
+            var randomBytes = new byte[n];
+            rand.NextBytes(randomBytes);
+            return randomBytes;
+        }
+        private void SayHelloButton_Click(object sender, EventArgs e)
+        {
+            byte[] rgb = GetRandomBytes(3);
+            textBox1.ForeColor = Color.FromArgb(255, rgb[0], rgb[1], rgb[2]);
+
+        }
+    }
+}
