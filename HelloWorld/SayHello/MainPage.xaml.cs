18234e38e29a12663fb1075e4ddd59bb1202372f
leeyunhome <leeyunhome@daum.net>
2020-02-29 14:53:32 +0900

    add SayHello_uwp

---
+++ HelloWorld/SayHello/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SayHello
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        Random rand;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            rand = new Random();
+        }
+
+        private byte[] GetRandomBytes(int n)
+        {
+            var randomBytes = new byte[n];
+            rand.NextBytes(randomBytes);
+            return randomBytes;
+        }
+
+        private void SayHelloButton_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] rgb = GetRandomBytes(3);
+
+            var randomColorBrush = new SolidColorBrush(Color.FromArgb(255, rgb[0], rgb[1], rgb[2]));
+
+            textBox1.BorderBrush = randomColorBrush;
+            textBox1.Foreground = randomColorBrush;
+        }
+    }
+}
